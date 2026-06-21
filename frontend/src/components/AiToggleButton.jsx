1566f0ae05a692dfbbec3521c890229816525ecf
Jeevan s <sathishjeevan2004@gmail.com>
2026-06-21 15:02:51 +0530

    aitoggle ui handled

--- frontend/src/components/AiToggleButton.jsx
+++ frontend/src/components/AiToggleButton.jsx
@@ -1,15 +1,22 @@
 import useAIassistantToggleStore from "@/store/useAIassistentToggleStore";
+import { IoSparklesSharp } from "react-icons/io5";
 
 const AiToggleButton = () => {
   const { toggleAiMode, setToggleAiMode } = useAIassistantToggleStore();
   return (
-    <div
-      className={`${toggleAiMode ? "bg-green-300 justify-end" : "bg-red-400 justify-start"} w-[50px] p-1 rounded-2xl flex flex-row `}
-    >
+    <div className="p-1 flex flex-row items-center gap-2 bg-gray-900 pl-3 pr-3 pt-2 pb-2 rounded-2xl border-2 border-black">
+      <div className="text-white flex flex-col text-center gap-1.5 items-center">
+        <IoSparklesSharp className="text-[15px] " />
+        <p className="text-[10px] text-gray-400"> Assistent mode</p>
+      </div>
       <div
-        onClick={() => setToggleAiMode()}
-        className="w-[20px] h-5 rounded-full bg-black"
-      ></div>
+        className={`${toggleAiMode ? " justify-end" : " justify-start"} w-[50px] p-1 rounded-2xl flex flex-row bg-black  transition-colors duration-300 `}
+      >
+        <div
+          onClick={() => setToggleAiMode()}
+          className={`w-[20px] h-5 rounded-full ${toggleAiMode ? "bg-green-400" : "bg-red-500"} hover:cursor-pointer`}
+        ></div>
+      </div>
     </div>
   );
 };

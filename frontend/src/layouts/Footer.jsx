c046df1c2959f0b5835ab957b21410211ec50081
Jeevan s <sathishjeevan2004@gmail.com>
2026-06-21 15:07:36 +0530

    footer handled with minimal design

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -1,7 +1,33 @@
+import { FaGithub, FaEnvelope, FaLeaf } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="w-full h-35  bg-gray-900 text-white flex items-center justify-center ">
-      <p className="text-sm font-medium">EcoMetric_AI</p>
+    <footer className="w-full h-13 bg-gray-900 text-white flex items-center justify-between px-4 border-t border-gray-700">
+      <div className="flex items-center gap-2">
+        <FaLeaf className="text-green-400 text-sm" />
+        <span className="text-xs font-semibold">EcoMetricAI</span>
+      </div>
+
+      <p className="text-[10px] text-gray-400">
+        Built by <span className="text-white">Jeevan</span>
+      </p>
+
+      <div className="flex items-center gap-3">
+        <a
+          href="mailto:ecometricai2@gmail.com"
+          className="text-gray-300 hover:text-white transition"
+        >
+          <FaEnvelope className="text-sm" />
+        </a>
+
+        <a
+          href="https://github.com/jeevan-sathish/EcoMetricAI.git"
+          target="_blank"
+          className="text-gray-300 hover:text-white transition"
+        >
+          <FaGithub className="text-sm" />
+        </a>
+      </div>
     </footer>
   );
 };

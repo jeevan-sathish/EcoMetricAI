4a3173599221e06697b03e5c33560422599ec954
Jeevan s <sathishjeevan2004@gmail.com>
2026-06-30 21:23:21 +0530

    speech buttons current state representation handled when active or clicked

--- frontend/src/components/SpeechBox.jsx
+++ frontend/src/components/SpeechBox.jsx
@@ -2,8 +2,11 @@ import { RiSpeakAiFill } from "react-icons/ri";
 import { MdPauseCircle } from "react-icons/md";
 import { GrResume } from "react-icons/gr";
 import { IoStopCircleSharp } from "react-icons/io5";
+import { useState } from "react";
 
 const SpeechBox = ({ suggestion }) => {
+  const [speechStatus, setSpeechStatus] = useState("");
+
   function handleSpeak() {
     const speech = new SpeechSynthesisUtterance(suggestion);
     speech.lang = "en-US";
@@ -11,22 +14,38 @@ const SpeechBox = ({ suggestion }) => {
     speech.pitch = 1;
     speech.volume = 1;
     window.speechSynthesis.speak(speech);
+    setSpeechStatus("speak");
   }
   function handlePause() {
     window.speechSynthesis.pause();
+    setSpeechStatus("pause");
   }
   function handleResume() {
     window.speechSynthesis.resume();
+    setSpeechStatus("resume");
   }
   function handleStop() {
     window.speechSynthesis.cancel();
+    setSpeechStatus("stop");
   }
   return (
     <div className="flex flex-row gap-4 p-2 text-[17px] text-gray-500 ">
-      <RiSpeakAiFill onClick={handleSpeak} className=" hover:text-green-400 " />
-      <MdPauseCircle onClick={handlePause} className=" hover:text-yellow-300" />
-      <GrResume onClick={handleResume} className=" hover:text-green-200" />
-      <IoStopCircleSharp onClick={handleStop} className=" hover:text-red-500" />
+      <RiSpeakAiFill
+        onClick={handleSpeak}
+        className={`${speechStatus === "speak" ? "text-green-400" : "text-gray-600"}`}
+      />
+      <MdPauseCircle
+        onClick={handlePause}
+        className={`${speechStatus === "pause" ? "text-green-400" : "text-gray-600"}`}
+      />
+      <GrResume
+        onClick={handleResume}
+        className={`${speechStatus === "resume" ? "text-green-400" : "text-gray-600"}`}
+      />
+      <IoStopCircleSharp
+        onClick={handleStop}
+        className={`${speechStatus === "stop" && "text-gray-600"}`}
+      />
     </div>
   );
 };
